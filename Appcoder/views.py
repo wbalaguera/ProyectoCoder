827afb3a41861ceda6674611e8d69b4b8b78423d
wbalaguera <124528041+wbalaguera@users.noreply.github.com>
2023-02-07 18:03:20 -0500

    Punto seguro models listo

---
+++ Appcoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from Appcoder.models import Curso
+
+#def curso(self):
+    #curso=Curso(nombre="Python",camada=23800)
+    #curso.save()
+    #doctexto=f'---> Curso {curso.nombre}, camada {curso.camada}'
+    #return HttpResponse(doctexto)#
+
+    
